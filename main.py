e2b5bd3759c572d65a1b4640b8822b923a1257a1
Artem Gusev <gus_art_22@mail.ru>
2024-05-29 10:35:54 +0300

    Add top read

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template, request
 import os
+import subprocess
+# flask --app main run
+
+def read_top_output():
+    process = subprocess.Popen(['top', '-b', '-n', '1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    if process.returncode != 0:
+        print(f"Error: {stderr.decode('utf-8')}")
+    else:
+        output = stdout.decode('utf-8')
+        return output
 
 class pr():
     def __init__(self, pid, tty, time, cmd):
@@ -8,26 +20,42 @@ class pr():
         self.time = time
         self.cmd = cmd
 
+# def parse_top(top):
+#     top_info = []
+#     tasks_count = []
+#     cpu = []
+#     mem = []
+#     swap = []
+#     tasks = []
+#     for i in range(len(top.split("\n"))):
+#         if i == 0:
+
+
 a = os.popen("ps", mode='r', buffering=-1)
 b = ""
 prs = []
 for i in a.readlines()[1:]:
     pd, ty, tm, cd = i.split()
     prs += [pr(pid=pd, tty = ty, time = tm, cmd = cd)]
+pd, ty, tm, cd = None, None, None, None
         
 # b = a.read()  
 print(b)
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def hello_world():
-#     return f"<p>{b}</p>"
-
-
+@app.route("/")
+def hello_world():
+    top_output = read_top_output()
+    b = ""
+    for i in top_output.split("\n"):
+        b += f"{i}<br/>"
+    # return f"<p>Go to /hello/name?key=abc</p>"
+    return f"<p>{b}<p>"
 
 @app.route('/hello/')
 @app.route('/hello/<name>/')
 def hello(name=None):
+    
     searchword = request.args.get('key', '')
     return render_template('hello.html', person=name, key=searchword, prs=prs)
--- os_system.py
+++ os_system.py
@@ -1,6 +1,44 @@
-import os
-# a = os.system("ps")
-a = os.popen("ps", mode='r', buffering=-1)
-b = a.read()
-for i in range(5):
-    print(b)
+# import os
+# # a = os.system("ps")
+# a = os.popen("ps", mode='r', buffering=-1)
+# b = a.read()
+# for i in range(5):
+#     print(b)
+
+# import time
+
+# # Печатаем начальные строки
+# print("111")
+# print("222")
+# print(333)
+
+# # Ждем немного времени, чтобы пользователь мог увидеть начальные строки
+# time.sleep(2)
+
+# # ANSI escape code для перемещения курсора на две строки вверх
+# UP_TWO_LINES = "\033[3A"
+
+# # ANSI escape code для очистки строки от курсора до конца строки
+# CLEAR_LINE = "\033[K"
+
+# # Перемещаем курсор на две строки вверх и перезаписываем строки
+# print(f"{UP_TWO_LINES}{CLEAR_LINE}444")
+# print(f"{CLEAR_LINE}555")
+
+import subprocess
+
+def read_top_output():
+    process = subprocess.Popen(['top', '-b', '-n', '1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    if process.returncode != 0:
+        print(f"Error: {stderr.decode('utf-8')}")
+    else:
+        output = stdout.decode('utf-8')
+        return output
+
+top_output = read_top_output()
+print(top_output)
+b = ""
+for i in top_output.split("\n"):
+    b += f"{i}<br/>"
